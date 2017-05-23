03ace99010cc25611f3ff2f23dfb231c1871b43e
wang <wang@wangdeMacBook-Air.local>
2017-05-23 14:52:02 +1200

    Startup

---
+++ Study Tracker/ChecklistItem.swift
@@ -0,0 +1,20 @@
+//
+//  ChecklistItem.swift
+//  Study Tracker
+//
+//  Created by wang on 23/05/17.
+//  Copyright © 2017 Unitec. All rights reserved.
+//
+
+import Foundation
+
+//the absolute minimum amount of stuff you need in order to make a new object
+class ChecklistItem {
+    var text = ""
+    var checked = false
+    
+    func toggleChecked() {
+        checked = !checked
+    }
+}
+
